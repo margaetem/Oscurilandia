173c4ff80a9f12f96438407763da2a17c34f7fdf
mlilloblanco <60747023+mlilloblanco@users.noreply.github.com>
2020-02-23 11:45:14 -0300

    se me habian olvidado los constructores ups!

--- Oscurilandia/src/cl/awakelab/firstLine/Huevo.java
+++ Oscurilandia/src/cl/awakelab/firstLine/Huevo.java
@@ -4,6 +4,18 @@ public class Huevo {
 	private int filaObjetivo;
 	private int columnaObjetivo;
 	private int puntajeObtenido;
+	
+	//constructores
+	public Huevo ( ) {
+	this.filaObjetivo = 0;
+	this.columnaObjetivo = 0;
+	this.puntajeObtenido = 0;
+	}
+	public Huevo (int filaObjetivo, int columnaObjetivo, int puntajeObtenido ) {
+	this.filaObjetivo = filaObjetivo;
+	this.columnaObjetivo = columnaObjetivo;
+	this.puntajeObtenido = puntajeObtenido;
+	}
 
 	// getters and setters
 	public int getFilaObjetivo() {
