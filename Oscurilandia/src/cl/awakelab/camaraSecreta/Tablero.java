812ace8992a8ccba427e5b4d2ba58b27fbe9edbd
Frost <Frost@LAPTOP-O6RN3KID>
2020-02-23 22:50:59 -0300

    modificacion clase tablero

--- Oscurilandia/src/cl/awakelab/camaraSecreta/Tablero.java
+++ Oscurilandia/src/cl/awakelab/camaraSecreta/Tablero.java
@@ -1,5 +1,17 @@
 package cl.awakelab.camaraSecreta;
 
-public class Tablero {
+import java.util.ArrayList;
 
+public class Tablero {
+    //constante
+    public final static int CAPACIDAD_POR_DEFECTO = 18;
+
+    //atrivutos
+    private List<Carro> tablero;
+    private List<Huevo> tablero;
+    
+    //constructor
+    public Tablero(List) {
+        this.tablero = new ArrayList<>()
+    }
 }
