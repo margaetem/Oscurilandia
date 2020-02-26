a275657e1f0d974a9bd6dced82beb46a6c452d62
AlexisRS93 <60710824+AlexisRS93@users.noreply.github.com>
2020-02-26 16:55:59 -0300

    update javadoc

--- Oscurilandia/src/cl/awakelab/ejec/Ejecutable.java
+++ Oscurilandia/src/cl/awakelab/ejec/Ejecutable.java
@@ -5,10 +5,13 @@ import java.util.Scanner;
 
 import cl.awakelab.camaraSecreta.*;
 import cl.awakelab.pks.Carro;
-
+/**
+ * Esta clase se utiliza para ejecutar el promgrama.
+ * @author Trinidad Gaete, Manuel Lillo, Alexis Ruiz.
+ *
+ */
 public class Ejecutable {
-	//Probando Github...
-
+    
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
 
@@ -24,7 +27,10 @@ public class Ejecutable {
 			break;
 		}
 	}
-
+	/**
+	 * Este metodo se utiliza para darle la bienvenida al jugador y lo intruduce al juego	 
+	 * 
+	 */
 	static void menu() {
 		Tablero.escribir("*** BIENVENIDO A  O S C U R I L A N D I A *** \n");
 		Tablero.escribir("¡FirstLine necesita de tu ayuda! \n");
@@ -33,7 +39,9 @@ public class Ejecutable {
 				+ " los que protegen la entrada del lugar. \n");
 		Tablero.escribir("Debemos evitar que se salgan con la suya!!! \n");
 	}
-
+	/**
+	 * Da una explicacion de la mision y le pregunta al usuario si acepta la mision.
+	 */
 	static void pegateLaMision() {
 		Scanner scanner = new Scanner(System.in);
 		Tablero.escribir("Tu misión es lanzar huevos a los vehículos \n"
@@ -52,36 +60,48 @@ public class Ejecutable {
 			break;
 		}
 	}
-
+	/**
+	 * instancia la clase tablero, crea todos los carros 
+	 * tambien explica los puntajes y ejecuta el menu.
+	 */
 	static void crearMatriz() {
-		Tablero app = new Tablero();
-		app.crearCarro();
-		Scanner scanner = new Scanner(System.in);
-		int contadorLanzamientos = 0;
-		Tablero.escribir("\nEstás en el campo de batalla junto a los FirstLine\n");
-		while (true) {
-			repeatMenu();
-			Tablero.escribir("Escoge lo que quieres hacer.");
-			int op = Tablero.pedirEntero(scanner, 1, 4);
-			System.out.println();
-			if (op == 1) {
-				app.lanzarHuevo();
-				app.mostrarMatriz(false);
-				Tablero.escribir("Su puntaje es: " + app.mostrarPuntaje(contadorLanzamientos));
-				contadorLanzamientos++;
-			} else if (op == 2) {
-				app.mostrarMatriz(true);
-				Tablero.escribir("Su puntaje es: " + app.mostrarPuntajeTotal());
-			} else if (op == 3) {
-				ArrayList<Carro> carros = app.getCarros();
-				for (Carro carro : carros) {
-					Tablero.escribir(carro.toString());
-				}
-			} else
-				break;
-		}
-	}
+            Tablero app = new Tablero();
+            app.crearCarro();
+            Scanner scanner = new Scanner(System.in);
+            int contadorLanzamientos = 0;
+            Tablero.escribir("\nEstás en el campo de batalla junto a los FirstLine\n");
+            Tablero.escribir("El puntaje se asigna así:\n" + 
+                            "Si le achuntas a una Kromi(K) tienes 3pts por casilla.\n" + 
+                            "Tendrás 10pts adicionales si te la piteas entera(Ocupa 3 casillas verticales)\n" + 
+                            "Si le achuntas a un Caguano(C) tienes 2pts por casilla.\n" + 
+                            "Tendrás 7pts adicionales si te la piteas (Ocupa 2 casillas horizontales)\n" + 
+                            "Si le achuntas a una Trupalla(T) tienes 1pt.\n");
+            while (true) {
+                    repeatMenu();
+                    Tablero.escribir("Escoge lo que quieres hacer.");
+                    int op = Tablero.pedirEntero(scanner, 1, 4);
+                    System.out.println();
+                    if (op == 1) {
+                            app.lanzarHuevo();
+                            app.mostrarMatriz(false);
+                            Tablero.escribir("Su puntaje es: " + app.mostrarPuntaje(contadorLanzamientos));
+                            contadorLanzamientos++;
+                    } else if (op == 2) {
+                            app.mostrarMatriz(true);
+                            Tablero.escribir("Su puntaje es: " + app.mostrarPuntajeTotal());
+                    } else if (op == 3) {
+                            ArrayList<Carro> carros = app.getCarros();
+                            for (Carro carro : carros) {
+                                    Tablero.escribir(carro.toString());
+                            }
+                    } else
+                            break;
+            }
+    }
 
+	/**
+	 * muestra el menu al usuario.
+	 */
 	static void repeatMenu() {
 		Tablero.escribir("Opciones:");
 		Tablero.escribir("1.- Lanzar Huevo");
