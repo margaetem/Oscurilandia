536faeab56f92011eb60c35958644827799326b3
Trinidad.gm <margaetem@users.noreply.github.com>
2020-02-26 15:07:39 -0300

    Se limpia ejecutable y se sube UML

--- Oscurilandia/src/cl/awakelab/ejec/Ejecutable.java
+++ Oscurilandia/src/cl/awakelab/ejec/Ejecutable.java
@@ -9,7 +9,6 @@ import cl.awakelab.pks.Carro;
 public class Ejecutable {
 
 	public static void main(String[] args) {
-
 		Scanner scanner = new Scanner(System.in);
 
 		menu();
@@ -23,18 +22,15 @@ public class Ejecutable {
 				Tablero.escribir("\nTe vai' a poner la capa?");
 			break;
 		}
-
 	}
 
 	static void menu() {
-		//Para ver si funciona
 		Tablero.escribir("*** BIENVENIDO A  O S C U R I L A N D I A *** \n");
 		Tablero.escribir("¡FirstLine necesita de tu ayuda! \n");
 		Tablero.escribir("Los villanos de la Camara Secreta tienen un plan para apodarse \n"
 				+ "de la ciudad… para llevarlo a cabo cuentan con la ayuda \nde sus secuaces los PKS,"
 				+ " los que protegen la entrada del lugar. \n");
 		Tablero.escribir("Debemos evitar que se salgan con la suya!!! \n");
-
 	}
 
 	static void pegateLaMision() {
@@ -54,7 +50,6 @@ public class Ejecutable {
 				Tablero.escribir("\nChao Perkin!!");
 			break;
 		}
-
 	}
 
 	static void crearMatriz() {
@@ -81,8 +76,7 @@ public class Ejecutable {
 				for (Carro carro : carros) {
 					Tablero.escribir(carro.toString());
 				}
-			}
-			else
+			} else
 				break;
 		}
 	}
@@ -94,37 +88,4 @@ public class Ejecutable {
 		Tablero.escribir("3.- Mostrar datos PKS");
 		Tablero.escribir("4.- Salir\n");
 	}
-
-//	public static void juego() {
-//		Scanner scanner = new Scanner(System.in);
-//		Tablero app = new Tablero();
-//		int contadorLanzamientos = 0;
-//		while (true) {
-//			repeatMenu();
-//			int op = Tablero.pedirEntero(scanner, 1, 6);
-//			System.out.println();
-//			if (op == 1) {
-//				app.crearCarro();
-//				Tablero.escribir("La matriz ha sido creada. puedes comenzar a lanzar huevos.");
-//			} else if (op == 2) {
-//
-//				app.lanzarHuevo();
-//				app.mostrarMatriz(false);
-//				Tablero.escribir("Su puntaje es: " + app.mostrarPuntaje(contadorLanzamientos));
-//				contadorLanzamientos++;
-//			} else if (op == 3) {
-//				app.mostrarMatriz(true);
-//				Tablero.escribir("Su puntaje es: " + app.mostrarPuntajeTotal());
-//			} else if (op == 4) {
-//				ArrayList<Carro> carros = app.getCarros();
-//				for (Carro carro : carros) {
-//					Tablero.escribir(carro.toString());
-//				}
-//
-//			}
-//
-//			else
-//				break;
-//		}
-//	}
 }
